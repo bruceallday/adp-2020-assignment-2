ef6683fc5c95ad79a11176d041133de20159e589
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-02-06 20:53:31 -0500

    refactor: board into 5x5 array with random populating

--- components/grid/grid.component.js
+++ components/grid/grid.component.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Text, View, TouchableOpacity, Image } from "react-native"
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import Layout from '../../constants/layout'
 import { styles } from './grid.styles'
+
 
 import GestureRecognizer, {
   swipeDirections
@@ -11,43 +12,74 @@ import Tile from '../tile/tile.component'
 
 const Grid = () => {
 
-    const [swipeText, setSwipeText] = useState()
+    // const board = new Array(25).fill(0)
+    const board = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]
 
-    const board = new Array(25).fill(0)
-
-    const onSwipeUp = (gestureState) => {
-        console.log('YOU SWIPED UP')
+    const randomNumber = (int) => {
+        let num = Math.floor(Math.random() * int)
+        return num
     }
 
-    const onSwipeDown = (gestureState) => {
-        console.log("YOU SWIPED DOWN")
-    }
+    const tileWidth = Layout.width / 5
 
-    const onSwipeLeft = (gestureState) => {
-        console.log("YOU SWIPED LEFT")
-    }
+    const onSwipe = (gestureName, gestureState) => {
+        const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections
 
-    const onSwipeRight = (gestureState) => {
-        console.log("YOU SWIPED RIGHT")
+        let initialX = gestureState.x0;
+        let initialY = gestureState.y0;
+
+        let i = Math.round((initialX - 0.5 * tileWidth) / tileWidth)
+        let j = Math.round((initialY - 0.5 * tileWidth) / tileWidth)
+
+        console.log(i, j);
+
+        switch(gestureName){
+            case SWIPE_UP:
+                console.log("SWIPE UP")
+                break;
+                
+            case SWIPE_DOWN:
+                console.log("Swiped down")
+                break;
+
+            case SWIPE_LEFT:
+                console.log("SWIPED LEFT")
+                break;
+
+            case SWIPE_RIGHT:
+                console.log("SWIPED RIGHT")
+                break;
+        }
     }
 
     const config = {
-        velocityThreshold: 0.3,
+        velocityThreshold: 0.01,
         directionalOffsetThreshold: 80
     };
     
     return (
       <GestureRecognizer
         style={styles.container}
-        onSwipeUp={onSwipeUp}
-        onSwipeDown={onSwipeDown}
-        onSwipeLeft={onSwipeLeft}
-        onSwipeRight={onSwipeRight}
+        onSwipe={onSwipe}
         config={config}
       >
-        {board.map((item, index) => (
+        {/*board.map((item, index) => (
           <Tile key={index} img={index} item={item} />
-        ))}
+        ))*/}
+
+        {board.map((row, index)=>(
+            row.map((key, j)=>(
+                <Tile key={j} img={randomNumber(10)} item={key} />
+                ))
+            ))
+        }
+
       </GestureRecognizer>
     );
 }
--- components/tile/tile.component.js
+++ components/tile/tile.component.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import { Text, View, TouchableOpacity, Image } from "react-native"
 import { styles } from '../tile/tile.styles'
 import { FOOD_IMAGES } from './tile.images'
-import { swipeDirections } from "react-native-swipe-gestures"
+import Layout from '../../constants/layout'
 // import Draggable from "react-native-draggable"
 
 
 const Tile = (props) => {
 
     const { img, item } = props;
-    
+
     return (
       <View style={styles.tile}>
           <Image
