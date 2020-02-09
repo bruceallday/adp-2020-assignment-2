176e504f445476033d6879fb8615d670d2394d74
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-02-08 20:35:57 -0500

    feat: added drag index to grid index location

--- components/tile/tile.component.js
+++ components/tile/tile.component.js
@@ -14,9 +14,32 @@ const Tile = (props) => {
 
     const { img, item, location } = props;
 
+    const tileWidth = Layout.width / 5
+
+    const dragIndex = (event) => {
+      
+        // console.log(event);
+        // console.log( "CURRENT X:" + event.touchHistory.touchBank[1].currentPageX);
+        // console.log( "CURRENT Y:" + event.touchHistory.touchBank[1].currentPageY);
+
+        let initialX = event.touchHistory.touchBank[1].currentPageX;
+        let initialY = event.touchHistory.touchBank[1].currentPageY;
+
+        let i = Math.round((initialX - 0.5 * tileWidth) / tileWidth)
+        let j = Math.round((initialY - 0.5 * tileWidth) / tileWidth)
+
+        console.log(i, j)
+    }
+
     return (
       <View style={styles.tile}>
-        <Draggable x={20} y={20} shouldReverse>
+        <Draggable
+          x={20}
+          y={20}
+          onDrag={event => dragIndex(event)}
+          shouldReverse
+
+        >
           <Image source={FOOD_IMAGES[img]} style={{ width: 40, height: 40 }} />
         </Draggable>
       </View>
